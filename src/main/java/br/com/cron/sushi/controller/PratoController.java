ab4be532507aec1bb35b7d3cd015216642f4d38d
Alex Oliveira <alexoliveirasince1989@hotmail.com>
2019-04-08 14:50:48 -0300

    Only images

--- src/main/java/br/com/cron/sushi/controller/PratoController.java
+++ src/main/java/br/com/cron/sushi/controller/PratoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.cron.sushi.Util.Disco;
 import br.com.cron.sushi.model.Prato;
